b3f2e2a7848ac410ae1c833d0051223fa631d2b1
Luke Hartman <ljhartman10@gmail.com>
2020-12-06 18:49:57 -0800

    day7 prep

---
+++ day07/part1.py
@@ -0,0 +1,10 @@
+def solve(f):
+    groups = f.split("\n\n")
+
+def main():
+    with open("input.txt") as f:
+        solve(f.read())
+
+
+if __name__ == "__main__":
+    main()
---
+++ day07/part2.py
@@ -0,0 +1,11 @@
+def solve(f):
+    groups = f.split("\n\n")
+
+def main():
+    with open("input.txt") as f:
+        solve(f.read())
+
+
+if __name__ == "__main__":
+    main()
+
